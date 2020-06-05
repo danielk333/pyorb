81a3ec6744cac522ea796c4eff3c50bf3c6d8567
danielk <daniel.kastinen@irf.se>
2020-06-05 17:16:40 +0200

    initial commit

---
+++ README.rst
@@ -0,0 +1,22 @@
+PyOrb
+=========
+
+Feature list
+-------------
+
+* 
+
+
+To install
+-----------------
+
+.. code-block:: bash
+
+   pip install pyorb
+
+Example
+---------------
+
+.. code-block:: python
+
+    import pyorb
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+
+with open('requirements', 'r') as fh:
+    pip_req = fh.read().split('\n')
+    pip_req = [x.strip() for x in pip_req if len(x.strip()) > 0]
+
+
+setuptools.setup(
+    name='pyorb',
+    version='0.0.0',
+    long_description=long_description,
+    url='https://github.com/danielk333/pyorb',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU-GPLv3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=pip_req,
+    packages=setuptools.find_packages(),
+    # metadata to display on PyPI
+    author='Daniel Kastinen',
+    author_email='daniel.kastinen@irf.se',
+    description='Python Kepler orbit functions',
+    license='GNU-GPLv3',
+)
